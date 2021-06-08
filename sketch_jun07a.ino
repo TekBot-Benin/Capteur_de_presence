3c007205bbceb65db379685e9f30cb6d81c92936
joris <ouedraogojoris47@gmail.com>
2021-06-08 20:36:33 +0100

    capteur

---
+++ sketch_jun07a.ino
@@ -0,0 +1,79 @@
+/* Constantes pour les broches */
+const byte TRIGGER_PIN = 2; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+int i = 0;
+ 
+/* Constantes pour le timeout */
+const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
+
+/* Vitesse du son dans l'air en mm/us */
+const float SOUND_SPEED = 340.0 / 1000;
+
+void setup() {
+   
+  /* Initialise le port série */
+  Serial.begin(9600);
+    pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);  
+  /* Initialise les broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+float distance()
+{
+/* 1. Lance une mesure de distance en envoyant une impulsion HIGH de 10µs sur la broche TRIGGER */
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  /* 2. Mesure le temps entre l'envoi de l'impulsion ultrasonique et son écho (si il existe) */
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+   
+  /* 3. Calcul la distance à partir du temps mesuré */
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+   
+  /* Affiche les résultats en mm, cm et m */
+  /*Serial.print(F("Distance: "));
+  Serial.print(distance_mm);
+  Serial.print(F("mm ("));
+  Serial.print(distance_mm / 10.0, 2);
+  Serial.print(F("cm, "));
+  Serial.print(distance_mm / 1000.0, 2);
+  Serial.println(F("m)"));*/
+  delay(500);
+ return (distance_mm);
+}
+
+void print_it()
+{
+
+  int distance_mm = distance();
+  if ((distance_mm / 2000.0) < 1) {
+    digitalWrite(7, HIGH);
+    digitalWrite(8, LOW);
+    tone(10, 220, 100);
+   i++;
+    Serial.print(i);
+    Serial.print("\n");
+    for(; (distance() / 2000) < 1;);
+    //distance_mm = distance();
+    //delayMicroseconds(10);
+   //
+  } else{
+    digitalWrite(7, LOW);
+    digitalWrite(8, HIGH);
+  }
+}
+
+void loop() {
+  
+  //while (1)
+  print_it();
+  /* Délai d'attente pour éviter d'afficher trop de résultats à la seconde */
+  
+  
+    
+}
